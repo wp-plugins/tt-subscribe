faeeba0bce23cafaa876965b8d06c6e82191d563
thesabeltuto <thesabeltuto@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-04-17 17:19:39 +0000

    TT-Subscribe Plugin V.1.0
    git-svn-id: https://plugins.svn.wordpress.org/tt-subscribe/trunk@699206 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ defaults.php
@@ -0,0 +1,13 @@
+<?php /* defaults */
+
+function tt_subscribe_defaults() {
+	
+	update_options('tt-subscribe-text', 'grab the latest issue now');
+	update_options('tt-subscribe-prefix', 'follow');
+	update_options('tt-subscribe-background', '6d84b4'); // #6d84b4, with no hastag
+	update_options('tt-subscribe-width', '280'); // 280px, with no px
+
+}
+
+
+?>
