b074e2b60ee6d1b6099b0595b54829ec16a10540
thesabeltuto <thesabeltuto@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-05-07 17:59:31 +0000

    === TT - Subscribe === Contributors: thesabeltuto Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=H228JQZP6269J&lc=PH&item_name=TT%2dPlugins&item_number=tt%2dplugins¤cy_code=USD&bn=PP%2dDonationsBF%3abtn_donateCC_LG%2egif%3aNonHosted Version: 2.0 Tags: tt-plugins, subscribe, facebook, google, twitter, feedburner, social Requires at least: 3.3 Tested up to: 3.5.1 Stable tag: 2.0 Author: Thesabel Tuto Author URI: http://thesabeltuto.blogspot.com Plugin URI: http://wordpress.org/extend/plugins/tt-subscribe/ License: GPLv2 or later License URI: http://www.gnu.org/licenses/gpl-2.0.html
    This plugin is a simplified subscription box that is totally customizable. It currently includes facebook, twitter, google plus and feedburner.
    
    == Description ==
    
    This plugin is a simplified subscription box that is totally customizable. It currently includes facebook, twitter, google plus and feedburner. Other social media links will be included soon. Created by Thesabel Tuto.
    
    **NEW!!!** Updates:
    1. Shortcodes: `[tt_subscribe]`
    2. Widget: `TT-Subscribe`
    3. Fixed layout on content area
    4. Customizable header color
    
    
    
    NOTICE FROM THE DEVELOPER
    1. If you want me to add more social media links here, please do suggest on the support area!
    2. If you do like this plugin, please do write a review!
    3. Thank you all for your patronage!
    4. This is a **FREE PLUGIN**!
    
    
    DISCLAIMER AREA
    
    Facebook Information:
    This plugin uses the current and simplified LIKE BUTTON of facebook.com. Security issues are handled by facebook. This plugin will not need any other  private information regarding your facebook account.
    
    Twitter Information:
    This plugin uses the current and simplified FOLLOW ME BUTTON of twitter.com. Security issues are handled by twitter. This plugin will not need any other  private information regarding your twitter account.
    
    Google Plus Information:
    This plugin uses the current and simplified PROFILE BADGE of google.com. Security issues are handled by google. This plugin will not need any other  private information regarding your google account.
    
    Feedburner Information:
    This plugin uses simple link of your feedburner.com. Security issues are handled by feedburner. This plugin will not need any other  private information regarding your feedburner account.
    
    == Installation ==
    
    1. Upload `tt-subscribe` folder to the `/wp-content/plugins/` directory
    2. Activate the plugin through the 'Plugins' menu in WordPress
    3. **NEW!!!** There are **3 ways** to include TT-Subscribe to your WordPress Site! Here are your Options:
    
    3.1. Place `<?php do_action('tt_subscribe'); ?>` in your templates
    3.2. **NEW!!!** Add shortcode `[tt_subscribe]` to your Posts or Pages.
    3.3. **NEW!!!** Drag and drop widget called `TT-Subscribe` to your Theme.
    
    == Frequently asked questions ==
    
    1. Where can I change the settings?
    A: You can change the settings in the Dashboard -> TT Subscribe menu.
    
    == Screenshots ==
    
    
    
    == Changelog ==
    
    1. A Shortcode has been added to this plugin.
    1.1. To add the shortcode to your Posts / Pages please include the code `[tt_subscribe]`
    2. A Widget has been added to this plugin.
    2.1. To add the widget to your Theme please drag and drop the `TT-Subscribe` widget to your desired location on the theme.
    
    == Upgrade notice ==
    
    **NEW!!!** Version 2.0 is Out!!!
    
    You can easily overwrite your existing TT-Subscribe plugin with this new one!
    
    git-svn-id: https://plugins.svn.wordpress.org/tt-subscribe/branches/v.2.0@709371 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ index.php
@@ -0,0 +1,88 @@
+<?php /* empty bottle is empty.*/ 
+
+/* README.TXT
+
+=== TT - Subscribe ===
+Contributors: thesabeltuto
+Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=H228JQZP6269J&lc=PH&item_name=TT%2dPlugins&item_number=tt%2dplugins¤cy_code=USD&bn=PP%2dDonationsBF%3abtn_donateCC_LG%2egif%3aNonHosted
+Version: 2.0
+Tags: tt-plugins, subscribe, facebook, google, twitter, feedburner, social
+Requires at least: 3.3
+Tested up to: 3.5.1
+Stable tag: 2.0
+Author: Thesabel Tuto
+Author URI: http://thesabeltuto.blogspot.com
+Plugin URI: http://wordpress.org/extend/plugins/tt-subscribe/
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+This plugin is a simplified subscription box that is totally customizable. It currently includes facebook, twitter, google plus and feedburner.
+
+== Description ==
+
+This plugin is a simplified subscription box that is totally customizable. It currently includes facebook, twitter, google plus and feedburner. Other social media links will be included soon. Created by Thesabel Tuto.
+
+**NEW!!!** Updates:
+1. Shortcodes: `[tt_subscribe]`
+2. Widget: `TT-Subscribe`
+3. Fixed layout on content area
+4. Customizable header color
+
+http://ttplugins.wordpress.com/tt-subscribe
+
+
+NOTICE FROM THE DEVELOPER
+1. If you want me to add more social media links here, please do suggest on the support area!
+2. If you do like this plugin, please do write a review!
+3. Thank you all for your patronage!
+4. This is a **FREE PLUGIN**!
+
+
+DISCLAIMER AREA
+
+Facebook Information:
+This plugin uses the current and simplified LIKE BUTTON of facebook.com. Security issues are handled by facebook. This plugin will not need any other  private information regarding your facebook account.
+
+Twitter Information:
+This plugin uses the current and simplified FOLLOW ME BUTTON of twitter.com. Security issues are handled by twitter. This plugin will not need any other  private information regarding your twitter account.
+
+Google Plus Information:
+This plugin uses the current and simplified PROFILE BADGE of google.com. Security issues are handled by google. This plugin will not need any other  private information regarding your google account.
+
+Feedburner Information:
+This plugin uses simple link of your feedburner.com. Security issues are handled by feedburner. This plugin will not need any other  private information regarding your feedburner account.
+
+== Installation ==
+
+1. Upload `tt-subscribe` folder to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. **NEW!!!** There are **3 ways** to include TT-Subscribe to your WordPress Site! Here are your Options:
+
+3.1. Place `<?php do_action('tt_subscribe'); ?>` in your templates
+3.2. **NEW!!!** Add shortcode `[tt_subscribe]` to your Posts or Pages.
+3.3. **NEW!!!** Drag and drop widget called `TT-Subscribe` to your Theme.
+
+== Frequently asked questions ==
+
+1. Where can I change the settings?
+A: You can change the settings in the Dashboard -> TT Subscribe menu.
+
+== Screenshots ==
+
+
+
+== Changelog ==
+
+1. A Shortcode has been added to this plugin.
+1.1. To add the shortcode to your Posts / Pages please include the code `[tt_subscribe]`
+2. A Widget has been added to this plugin.
+2.1. To add the widget to your Theme please drag and drop the `TT-Subscribe` widget to your desired location on the theme.
+
+== Upgrade notice ==
+
+**NEW!!!** Version 2.0 is Out!!!
+
+You can easily overwrite your existing TT-Subscribe plugin with this new one!
+
+*/
+?>
