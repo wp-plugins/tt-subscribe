d575404a9b00067749c0fed8db26d15a391e1502
thesabeltuto <thesabeltuto@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-10 08:40:00 +0000

    Version 4.0.2
    git-svn-id: https://plugins.svn.wordpress.org/tt-subscribe/tags/4.0.2@1109302 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ framework/admin.php
@@ -0,0 +1,9 @@
+<?php /* admin page */
+require_once(TT_SUBSCRIBE_DIR.'framework/admin/settings.php');
+require_once(TT_SUBSCRIBE_DIR.'framework/admin/home.php');
+require_once(TT_SUBSCRIBE_DIR.'framework/admin/basic.php');
+require_once(TT_SUBSCRIBE_DIR.'framework/admin/subscriptions.php');
+require_once(TT_SUBSCRIBE_DIR.'framework/admin/advance.php');
+require_once(TT_SUBSCRIBE_DIR.'framework/admin/support.php');
+require_once(TT_SUBSCRIBE_DIR.'framework/admin/about.php');
+?>
